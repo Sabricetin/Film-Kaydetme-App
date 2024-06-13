5be20e77289797e50616eef6c1c720274820f65c
Sabricetin <sabriyazilim@gmail.com>
2024-06-13 23:51:19 +0300

    final versiyon

---
+++ Film-Kaydetme-APP/DetailsVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsVC.swift
+//  Film-Kaydetme-APP
+//
+//  Created by Sabri Çetin on 14.05.2024.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+}
