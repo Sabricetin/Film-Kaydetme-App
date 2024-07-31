ca4fcc9362ca4b762fb3942e6c6bea903c10c78e
Sabricetin <sabriyazilim@gmail.com>
2024-07-31 15:37:41 +0300

    Constraints were made

--- Film-Kaydetme-APP/DetailsVC.swift
+++ Film-Kaydetme-APP/DetailsVC.swift
@@ -6,15 +6,146 @@
 //
 
 import UIKit
+import PhotosUI
+import CoreData
 
-class DetailsVC: UIViewController {
-
+class DetailsVC: UIViewController , UINavigationControllerDelegate , PHPickerViewControllerDelegate  {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var konuText: UITextField!
+    @IBOutlet weak var sureText: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var chosenFilm = ""
+    var chosenFilmId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if chosenFilm != "" {
+            
+            //buton'un tıklanıp tıklanmamasını sağlar
+            saveButton.isEnabled = false
+            
+            //CoreData'dan Veri Çekip Göstermek
+            
+            let appDelegete = UIApplication.shared.delegate as! AppDelegate
+            let context  = appDelegete.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idStiring  = chosenFilmId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idStiring!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "filmadi") as? String {
+                            nameText.text = name
+                        }
+                        if let konu = result.value(forKey: "konusu") as? String {
+                            konuText.text = konu
+                        }
+                        if let sure = result.value(forKey: "sure") as? Int {
+                            sureText.text = String(sure)
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                    }
+                }
+            } catch {
+                print ("error")
+            }
+        }
+        else {
+            saveButton.isHidden = false
+            saveButton.isEnabled = false
+            nameText.text = ""
+            konuText.text = ""
+            sureText.text = ""
+        }
+
+        //Recognizer
+        
+        let gesturRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gesturRecognizer)
+
+        imageView.isUserInteractionEnabled  = true
+        let imageTapRecegnizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecegnizer)
     }
     
+    //Fotağrafın tıklanıp galeriye gitmesini sağlayan kod
+    
+    @objc func selectImage () {
+        
+        var configuraation = PHPickerConfiguration()
+        configuraation.filter = .images
+        let picker = PHPickerViewController(configuration: configuraation)
+        picker.delegate = self
+        present ( picker , animated: true)
 
-  
+    }
+     // seçtiğimiz görsel imageView'de gösterilmesini sağlar
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        saveButton.isEnabled = true
+        guard !results.isEmpty else {
+                    return
+                }
+        if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self)  { [weak self] (image, error) in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                } else if let image = image as? UIImage {
+                    DispatchQueue.main.async {
+                        self?.imageView.image = image // UIImageView'e seçilen görüntüyü atama
+                    }
+                }
+            }
+        }
+    }
+    // Ekranın herhangi bir yerine tıkladığında klavye kapanmasını sağlar
+    
+    @objc func hideKeyboard () {
+        view.endEditing(true)
+    }
+    
+    // Veriyi Core Data'ya Saklama İşlemi
+    
+    @IBAction func kaydet(_ sender: Any) {
+        
+        let applDelegete = UIApplication.shared.delegate as! AppDelegate
+        let context = applDelegete.persistentContainer.viewContext
+        let newFilm = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //Attributes / Öznitellikler
+        
+        newFilm.setValue(nameText.text!, forKey: "filmadi")
+        newFilm.setValue(konuText.text!, forKey: "konusu")
+        if let sure = Int(sureText.text!) {
+            newFilm.setValue(sure , forKey: "sure")
+        }
+        newFilm.setValue(UUID(), forKey: "id")
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        newFilm.setValue(data, forKey: "image")
+        
+        do  {
+            try context.save()
+            print("sucsess")
+        }
+        catch {
+            print("error")
+        }
+        
+        //View Controller Arası Mesaj Göndermeye Sağlayan Kod
+        
+        NotificationCenter.default.post (name: NSNotification.Name("newData") , object: nil)
+        self.navigationController?.popViewController(animated: true)
+    }
 }
--- Film-Kaydetme-APP/ViewController.swift
+++ Film-Kaydetme-APP/ViewController.swift
@@ -6,14 +6,136 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+        
+    var namArray = [String] ()
+    var idArray = [UUID]()
+    var selectedFilm = ""
+    var selectedFilmId : UUID?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //navigasyon barına "artı-işareti" koymak için kullandığımız kod
+        
+             navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem (barButtonSystemItem:   UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        getData()
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    // Verileri Çekme Komutu
+    
+  @objc  func getData () {
+      
+      //Aynı Verileri TableView'a Kaydetememek İçin 1defa Döndürüyor
+      
+      namArray.removeAll(keepingCapacity: false)
+      idArray.removeAll(keepingCapacity: false)
+        
+        let appDelegete = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegete.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+      
+        do {
+         let results =  try  context.fetch(fetchRequest)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "filmadi") as? String {
+                        self.namArray.append(name)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    //TableView Yeni Data Geldiği Zaman Güncelleme İşlemi
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        catch {
+            print ("error")
+        }
+    }
+    @objc  func addButtonClicked () {
+        selectedFilm = ""
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    //TableView Ayarları
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return namArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell ()
+        var content = cell.defaultContentConfiguration()
+        content.text = namArray[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    //View Controller Arası Segue Yapma
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.chosenFilm = selectedFilm
+            destinationVC.chosenFilmId = selectedFilmId
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        selectedFilm = namArray[indexPath.row]
+        selectedFilmId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    //Core-Data-Delete-silme-İşlemi
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            let appDelegete = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegete.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = idArray[indexPath.row] .uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+    
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == idArray[indexPath.row] {
+                                context.delete(result)
+                                namArray.remove(at: indexPath.row)
+                                idArray .remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                do {
+                                    try context.save()
+                                } catch {
+                                    print ("error!!!")
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+            catch {
+                print ("error!!!")
+            }
+        }
+    }
 }
-
